4652f69c7aa850471eea0862c5efaaebdbe23e7f
sayanmaity931 <sayanmaity931@gmail.com>
2025-06-21 16:41:59 +0530

    Few Modification

--- app/src/main/java/com/example/maps/MapScreen.kt
+++ app/src/main/java/com/example/maps/MapScreen.kt
@@ -19,7 +19,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -41,10 +43,13 @@ fun MapScreen() {
     val context = LocalContext.current
 
     val placesClient = Places.createClient(context)
-    var searchQuery by remember { mutableStateOf("") }
+    var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
     var predictions by remember { mutableStateOf<List<AutocompletePrediction>>(emptyList()) }
     val atasehir by remember {  mutableStateOf( LatLng(22.5744, 88.3629) )}
     val markerState by remember { mutableStateOf(MarkerState(position = atasehir)) }
+    val focusManager = LocalContext.current
+        .let { androidx.compose.ui.platform.LocalFocusManager.current }
+
 
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(atasehir, 15f)
@@ -56,7 +61,7 @@ fun MapScreen() {
             value = searchQuery,
             onValueChange = { query ->
                 searchQuery = query
-                fetchPlacePredictions(query, placesClient) { newPredictions ->
+                fetchPlacePredictions(query.text, placesClient) { newPredictions ->
                     predictions = newPredictions
                 }
             },
@@ -67,7 +72,7 @@ fun MapScreen() {
                 .padding(8.dp),
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
             keyboardActions = KeyboardActions(onSearch = {
-                // Handle search action here if needed
+
                 }
             )
         )
@@ -75,9 +80,15 @@ fun MapScreen() {
         // Display Search Suggestions
         predictions.forEach { prediction ->
             TextButton(onClick = {
-                searchQuery = prediction.getPrimaryText(null).toString()
+                searchQuery = TextFieldValue(
+                    text = prediction.getPrimaryText(null).toString(),
+                    selection = TextRange(prediction.getPrimaryText(null).toString().length)
+                )
                 moveCameraToLocation(prediction.placeId, placesClient, cameraPositionState , markerState )
                 predictions = emptyList()
+
+                focusManager.clearFocus()
+
                     }
                 ) {
                 Text(prediction.getPrimaryText(null).toString())
